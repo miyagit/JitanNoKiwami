002a3db6e3f15ef9fc1d39ed8190b4c8d0ad4f01
koudaimurakami <hakmd.m.98@gmail.com>
2024-05-08 00:12:33 +0900

    出題問題のデータ取得ロジックをコントローラに実装

--- app/controllers/category_quiz_questions_controller.rb
+++ app/controllers/category_quiz_questions_controller.rb
@@ -1,5 +1,13 @@
 class CategoryQuizQuestionsController < ApplicationController
+  
   def index
     @category_quiz_questions = CategoryQuizQuestion.order("RAND()").limit(10)
+    @questions_with_choices = @category_quiz_questions.map do |question|
+      correct_choices = question.category_question_choices.where(is_correct: true)
+      incorrect_choices = question.category_question_choices.where(is_correct: false).shuffle.take(3)
+      choices_to_display = correct_choices + incorrect_choices
+      { question: question, choices: choices_to_display }
+    end
   end
+  
 end
